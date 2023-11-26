a755fa7176d5e53c18e0824cc54f338d5338a337
Alex Gaetano Padula <apadula@cursusdb.com>
2023-11-26 18:05:55 -0500

    updated documentation

--- src/routes/documentation/+page.svelte
+++ src/routes/documentation/+page.svelte
@@ -32,19 +32,23 @@
 <aside transition:slide={{ delay: 250, duration: 300, easing: quintOut, axis: 'x' }}>
     <input bind:this={searchInput} type="search" placeholder="search docs.. CTRL+f" />
     <ul>
-        <li>Getting Started</li>
-        <!--        <li>Query Language</li>-->
+        <li><a href="#getting-started">Getting Started</a></li>
+        <li><a href="#setting-up-cluster">Setting Up Cluster</a></li>
+        <li><a href="#setting-up-node">Setting Up / Starting Node</a></li>
+        <li><a href="#connecting-to-cluster">Connecting To Cluster</a></li>
+        <li><a href="#tls-with-certbot">Setup TLS With Certbot</a></li>
+        <li><a href="#query-language">Query Language</a></li>
     </ul>
 </aside>
 {/if}
 
 <main>
-    <article style={`${clientWidth < 746 ? "padding: 0;" : "padding-left: 240px;"}`}>
+    <article style={`${clientWidth < 746 ? "padding-left: 0;" : "padding-left: 240px;"}`}>
         {#if clientWidth < 746 }
             <button class="aside-btn" on:click={() => showAside ? showAside = false : showAside = true}><img src="aside-toggle.png" /></button>
         {/if}
-        <h1>Getting Started</h1><br/>
-        <h3>Setting Up Cluster</h3>
+        <h1 id="getting-started">Getting Started</h1><br/>
+        <h3 id="setting-up-cluster">Setting Up Cluster</h3>
         <p>First download the latest version of the CursusDB cluster from <a href="/downloads">Here</a> for your operating system.</p>
         <p>Once downloaded Cursus will expect an initial database user and key to be created upon first start up.</p>
         <Prism language="bash" code={`
@@ -61,7 +65,7 @@ tls: false
 port: 7681
 users:
     - DX8EAQL/gAABDAEQAABO/4AAAwh1c2VybmFtZQZzdHJpbmcMBgAEYWxleAhwYXNzd29yZAZzdHJpbmcMCAAGcGFkdWxhCnBlcm1pc3Npb24Gc3RyaW5nDAQAAlJX
-`} header=".cursusconfig"/>
+`}/>
 
         <p>Your initial user has <strong>RW</strong> which is READWRITE permissions.  You can see in your .cursusconfig that you have a serialized user which is encoded to be stored securely and taking up little room.</p>
 
@@ -81,7 +85,7 @@ tls: false
 port: 7681
 users:
     - DX8EAQL/gAABDAEQAABO/4AAAwh1c2VybmFtZQZzdHJpbmcMBgAEYWxleAhwYXNzd29yZAZzdHJpbmcMCAAGcGFkdWxhCnBlcm1pc3Npb24Gc3RyaW5nDAQAAlJX
-`} header=".cursusconfig"/>
+`} />
 
         <p>You might be thinking ok, but what about the node I don't have it yet.</p>
         <p>Not to worry.  You can download the <strong>curode</strong> which is the Cursus cluster node from <a href="/downloads">Here</a></p>
@@ -106,7 +110,7 @@ users:
 `}/><br/>
 
 
-        <h3>Starting Node</h3>
+        <h3 id="setting-up-node">Setting up / Starting Node</h3>
         <p>You can choose to start your node with a different port than whats in .curodeconfig using --port flag.</p>
         <img style="filter: invert(1);" src="docs/cluster-setup3.png" /><br/>
 
@@ -117,7 +121,7 @@ users:
 
 
 
-        <h3>Connecting To Cluster</h3><br/>
+        <h3 id="connecting-to-cluster">Connecting To Cluster</h3><br/>
         <h4>Connect via curush (CursusDB Shell Program)</h4>
         <p><a href="/downloads">Download curush</a></p>
 
@@ -136,7 +140,7 @@ users:
         <p>coming soon.</p>
         <br/><br/>
 
-        <br/><h2>TLS Cert with Certbot</h2>
+        <br/><h2 id="tls-with-certbot">TLS with Certbot</h2>
         <p><strong>Ubuntu Example</strong></p>
         <p>If you have any Certbot packages installed using an OS package manager like apt, ect, you should remove them before installing the Certbot snap.  This ensures that when you run the command certbot the snap is used rather than the installation from your OS package manager.</p>
 
@@ -182,7 +186,7 @@ If you like Certbot, please consider supporting our work by:
 * Donating to ISRG / Let's Encrypt:   https://letsencrypt.org/donate
 * Donating to EFF:                    https://eff.org/donate-le
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-`} header="Certbot response"/>
+`} />
 
 
         <p>Now with your cert and key ready you can setup your <strong>.cursusconfig</strong> and or <strong>.curodeconfig</strong></p>
@@ -195,17 +199,93 @@ tls: true
 port: 7681
 users:
     - DX8EAQL/gAABDAEQAABO/4AAAwh1c2VybmFtZQZzdHJpbmcMBgAEYWxleAhwYXNzd29yZAZzdHJpbmcMCAAGcGFkdWxhCnBlcm1pc3Npb24Gc3RyaW5nDAQAAlJX
-`} header=".cursusconfig"/>
+`} />
 
         <p>Now when your cluster is secured with TLS every attempt to connect with Curush or a native package you must make sure to enable TLS.</p>
 
         <Prism language="bash" code={`
  ./curush --host=X --port=X --tls=true
 `}/><br/>
+
+        <br/><h2 id="query-language">Query Language</h2>
+        <p>CursusDB's Query Language is very similar to that of SQL but some slight modifications.</p>
+
+        <p><strong>Pointers</strong></p>
+        <ul>
+            <li>End every query with a semi colon.</li>
+            <li>Nested objects are not part of CursusDB's search algorithms.</li><br/>
+        </ul>
+        <h3>Collections</h3>
+        <p>When inserting a document to a non-existent collection, the collection will be created on the node.</p>
+        <p>A collection is like a labeled cabinet where your documents are stored.</p>
+
+
+        <h3>Inserting Documents</h3>
+        <p>Inserting documents is very easy.  CursusDB takes JSON within the insert's parenthesis like so:</p>
+        <Prism language="sql" code={`
+insert into users({"name": "Alex", "last": "Lee", "age": 28});
+`}/><br/>
+
+        <p>The query above inserts the document into a users collection.  Documents within a collection on CursusDB are unstructured/schemaless meaning I could insert the next document like so:</p>
+        <Prism language="sql" code={`
+insert into users({"name": "John", "last": "Josh", "age": 28, "tags": ["tag1", "tag2"]});
+`}/><br/>
+
+        <p>Specifying new keys no problem!</p>
+
+        <p><strong>What about uniqueness across all nodes?</strong></p>
+        <p>CursusDB has this covered.  When inserting a document you can use an exclamation mark after your key name like so: </p>
+        <Prism language="sql" code={`
+insert into users({"name!": "John", "last": "Josh", "age": 28, "tags": ["tag1", "tag2"]});
+`}/><br/>
+        <p>Now on insert CursusDB will check every node if there is a document within the users collection with a key of "name" and value of "John".  You can specify multiple unique keys, even within an array!</p>
+
+
+        <h3>Selecting Documents</h3>
+        <Prism language="sql" code={`
+select * from users;
+select 0,2 from users;
+select 2,3 from users;
+select 4,5 from users;
+select 5,* from users;
+select 1 from users where name == 'Alex' || name == 'John';
+select * from users where name == 'Alex' && age == 28;
+select * from users where tags == "tag1";
+select * from users where name == 'Alex' && age == 28 && tags == 'tag1';
+`}/><br/>
+        <h3>Deleting Documents</h3>
+        <Prism language="sql" code={`
+delete * from users where age >= 28 || age < 32;
+delete 10,* from users where age > 28 && name == 'Alex';
+ect
+`}/><br/>
+        <h3>Updating Documents</h3>
+        <Prism language="sql" code={`
+update 1 in users where age >= 28 set name = 'Josie';
+update * in users where age > 24 && name == 'Alex' set name = 'Josie', age = 52;
+update n, n..
+ect..
+`}/><br/>
     </article>
 </main>
 
 <style>
+    h1 {
+        font-size: 28px;
+        color: rgb(53, 41, 80);
+    }
+
+    h2 {
+        color: rgb(53, 41, 80);
+        font-size: 22px!important;
+        font-weight: lighter!important;
+    }
+
+    h3 {
+        color: rgb(53, 41, 80);
+        font-weight: lighter;
+        font-size: 18px;
+    }
 
     img {
         border: 4px solid rgba(255,255,255,0.1);
@@ -267,12 +347,12 @@ users:
 
     aside ul li {
         padding: 10px 5px;
-        border-bottom: 1px solid #b7b7b7;
+        border-bottom: 1px solid #dcdcdc;
         cursor: pointer;
     }
 
     aside ul li:hover {
-        background:  #b7b7b7;
+        background: #dcdcdc;
     }
 
     aside ul li:last-child {
@@ -295,5 +375,14 @@ users:
         opacity: 0.74;
         margin-top: 7px;
         width: 22px;
+    }
+
+    article ul {
+        padding-left: 28px;
+    }
+
+    aside ul li a {
+        text-decoration: none;
+        border: none;
     }
 </style>
