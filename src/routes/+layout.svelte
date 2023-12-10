265dbd7edddcb98581718cdecc01af1ce964364c
Alex Gaetano Padula <apadula@cursusdb.com>
2023-12-10 08:35:29 -0500

    update curush version

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
     onMount(function() {
         currentVersionCursus.set("v1.6.5")
         currentVersionCurode.set("v1.6.5")
-        currentVersionCurush.set("v1.0.1")
+        currentVersionCurush.set("v1.1.0")
     })
 </script>
 
