7ad437cdf58e12f206c6065ddeaf528098a3006f
Alex Gaetano Padula <apadula@cursusdb.com>
2023-12-23 14:43:02 -0500

    bump cluster and node to v1.7.8

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,8 @@
     import {currentVersionCursus, currentVersionCurode,currentVersionCurush} from '$lib/stores.js'
 
     onMount(function() {
-        currentVersionCursus.set("v1.7.7")
-        currentVersionCurode.set("v1.7.7")
+        currentVersionCursus.set("v1.7.8")
+        currentVersionCurode.set("v1.7.8")
         currentVersionCurush.set("v1.1.0")
     })
 </script>
