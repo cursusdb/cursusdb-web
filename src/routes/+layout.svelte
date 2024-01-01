165392d64396aca4573bf1860f8ea26c15aa6448
Alex Gaetano Padula <apadula@cursusdb.com>
2024-01-01 00:46:08 -0500

    update cluster and node to v2.0.4

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,8 @@
     import {currentVersionCursus, currentVersionCurode,currentVersionCurush} from '$lib/stores.js'
 
     onMount(function() {
-        currentVersionCursus.set("v2.0.3")
-        currentVersionCurode.set("v2.0.3")
+        currentVersionCursus.set("v2.0.4")
+        currentVersionCurode.set("v2.0.4")
         currentVersionCurush.set("v1.1.3")
     })
 </script>
