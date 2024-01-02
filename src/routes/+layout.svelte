845b14fc68802c0f631bdc91dd352741e77ff543
Alex Gaetano Padula <apadula@cursusdb.com>
2024-01-02 01:53:19 -0500

    update curode and cursus version to v2.1.0

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,8 @@
     import {currentVersionCursus, currentVersionCurode,currentVersionCurush} from '$lib/stores.js'
 
     onMount(function() {
-        currentVersionCursus.set("v2.0.7")
-        currentVersionCurode.set("v2.0.7")
+        currentVersionCursus.set("v2.1.0")
+        currentVersionCurode.set("v2.1.0")
         currentVersionCurush.set("v1.1.3")
     })
 </script>
